863e6f38fe700917cc317a8f6df5481f00bdeca5
Sebastián Gomez <sebastianga48@gmail.com>
2026-08-24 12:00:15 -0600

    Validate RPC health query kind

--- apps/api/src/soroban/rpc-health.controller.ts
+++ apps/api/src/soroban/rpc-health.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Query } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Post, Query } from '@nestjs/common';
 import { RpcClientService, RpcEndpointKind, RpcEndpointState } from './rpc-client.service';
 
 @Controller('rpc')
@@ -7,12 +7,22 @@ export class RpcHealthController {
 
   @Get('health')
   getHealth(@Query('kind') kind?: RpcEndpointKind): RpcEndpointState[] {
-    return this.rpcClient.getSnapshot(kind);
+    return this.rpcClient.getSnapshot(this.parseKind(kind));
   }
 
   @Post('health/refresh')
   async refreshHealth(@Query('kind') kind?: RpcEndpointKind): Promise<RpcEndpointState[]> {
     await this.rpcClient.refreshHealth();
-    return this.rpcClient.getSnapshot(kind);
+    return this.rpcClient.getSnapshot(this.parseKind(kind));
+  }
+
+  private parseKind(kind?: string): RpcEndpointKind | undefined {
+    if (!kind) {
+      return undefined;
+    }
+    if (kind === 'soroban' || kind === 'horizon') {
+      return kind;
+    }
+    throw new BadRequestException('kind must be either "soroban" or "horizon"');
   }
 }
